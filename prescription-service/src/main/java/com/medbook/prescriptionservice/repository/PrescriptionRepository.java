eac19f2dbfe673512c2e60c07db2546bb310a104
vunguyen26 <nguyenminhvu591@gmail.com>
2025-10-12 22:21:52 +0700

    feat(prescription-service): implement CRUD APIs and fix issuedAt column issue

--- prescription-service/src/main/java/com/medbook/prescriptionservice/repository/PrescriptionRepository.java
+++ prescription-service/src/main/java/com/medbook/prescriptionservice/repository/PrescriptionRepository.java
@@ -1,11 +1,14 @@
-package com.medbook.prescriptionservice.repository;
+package com.medbook.prescriptionsservice.repository;
 
-import com.medbook.prescriptionservice.model.Prescription;
+import com.medbook.prescriptionsservice.model.Prescription;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface PrescriptionRepository extends JpaRepository<Prescription, Long> {
+
     List<Prescription> findByMedicalRecordId(Long medicalRecordId);
+
     List<Prescription> findByPatientId(Long patientId);
+
     List<Prescription> findByDoctorId(Long doctorId);
 }
